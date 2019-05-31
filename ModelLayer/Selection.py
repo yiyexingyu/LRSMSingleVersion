ae6ca1b99f7b10691d23e27fae839c1bf9810aa7
yiyexingyu <2958029539@qq.com>
2019-05-31 15:50:02 +0800

    .

---
+++ ModelLayer/Selection.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/30 21:57
+# @Author  : 何盛信
+# @Email   : 2958029539@qq.com
+# @File    : Selection.py
+# @Project : pyqt5_project
+# @Software: PyCharm
+
+""" 描述 """
--- UILayer/Workbench/RectangleItem.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-
-
-class RectangleItem(QGraphicsRectItem):
-
-    def __init__(self, parent=None):
-        super(RectangleItem, self).__init__(parent)
